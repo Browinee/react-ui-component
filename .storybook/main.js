cf919b0d05d24eb9f336f8a644b3e22b637ceee5
Browinee <nihilitypeo@gmail.com>
2023-03-02 19:32:15 +0800

    build: fix  storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,18 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    {
+      name: "@storybook/preset-create-react-app",
+      options: {
+        scriptsPackageName: 'react-scripts'
+      }
+    }
   ],
   "framework": "@storybook/react",
   "core": {
