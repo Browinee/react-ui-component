38392a21b80a48d4d6f43480759e0a75a74c5e68
User <user@AdmindeMacBook-Air.local>
2023-09-21 10:33:53 +0800

    refactor: refactor basic react-dnd folder

---
+++ src/dnd/basic/dnd.stories.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef, useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import App from ".";
+import { useDrag, useDrop, DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+export default {
+  title: "Example/react-dnd",
+  component: App,
+} as ComponentMeta<any>;
+
+const Template: ComponentStory<any> = (args) => (
+  <DndProvider backend={HTML5Backend}>
+    <App {...args}></App>
+  </DndProvider>
+);
+
+export const Basic = Template.bind({});
+Basic.args = {};
